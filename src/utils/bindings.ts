b64c60e87cf844562665394d68e14f31627ba89d
tkgstrator <nasawake.am@gmail.com>
2024-09-24 06:35:21 +0000

    feat: add discord token

--- src/utils/bindings.ts
+++ src/utils/bindings.ts
@@ -4,4 +4,7 @@ export type Bindings = {
   Resource: KVNamespace
   Cache: KVNamespace
   JWT_SECRET_KEY: string
+  DISCORD_CLIENT_ID: string
+  DISCORD_CLIENT_SECRET: string
+  DISCORD_GUILD_ID: string
 }
